0ece182f9194a691796ec4e4dc9b4106653a3458
Sreehari-4Git <sreeharibijukumar0acc4pub@gmail.com>
2026-05-07 00:18:26 +0530

    router-Applications file added and bug fix in database

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
-SQLALCHEMY_DATABASE_URL = "sqlite:///./jobportal.db"
+DATABASE_URL = "sqlite:///./jobportal.db"
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
