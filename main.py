41a7ac0b8d720d557029a6ad4b7fcf32e02829d7
gouri-ctrl <gourinandhana007@gmail.com>
2026-05-07 09:54:30 +0530

    main added

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 from database import engine, Base
 from routers import auth
 
-#eathanu sherikkum olla SQL Tables ninghalude model vechu indakkunnathu
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-#auth.py nirmicha routes upayogikkan fastapi node parayum
+@app.get("/")
+def read_root():
+    return {"message": "The Job portal is running!"}
+
 app.include_router(auth.router, prefix="/auth")
