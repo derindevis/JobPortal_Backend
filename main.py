57931b7698f55f8d9f3fff42c2c125fb446a65b3
derindevis <derindevis79@gmail.com>
2026-05-07 19:11:08 +0530

    updated

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI
 from database import engine, Base
-from routers import jobs,auth
-import models.job
-
+from routers import jobs,auth,applications
 #eathanu sherikkum olla SQL Tables ninghalude model vechu indakkunnathu
 Base.metadata.create_all(bind=engine)
 app = FastAPI(title="JobPortal")
 #auth.py nirmicha routes upayogikkan fastapi node parayum
 app.include_router(auth.router, prefix="/auth",tags=["Authentication"]) 
 app.include_router(jobs.router,prefix="/jobs",tags=["Jobs"])
+app.include_router(applications.router,prefix="/app",tags=["Applications"])
 @app.get("/")
 def read_root():
     return {"message": "The Job portal is running!"}
--- models/application.py
+++ models/application.py
@@ -5,11 +5,11 @@ from database import Base
 class Application(Base):
     __tablename__="applications"
     #[cite:16] use ariyilla nokkanam
-    id=Column(Integer, primary_key=True, index=True, autoincrement=True) [cite:16]
-    job_id=Column(Integer, ForeignKey("jobs.id"), nullable=False) [cite:16]
-    user_id=Column(Integer, ForeignKey("user.id"), nullable=False) [cite:16]
-    cover_letter=Column(Text, nullable=False) [cite:16]
+    id=Column(Integer, primary_key=True, index=True, autoincrement=True) 
+    job_id=Column(Integer, ForeignKey("jobs.id"), nullable=False) 
+    user_id=Column(Integer, ForeignKey("users.id"), nullable=False) 
+    cover_letter=Column(Text, nullable=False)
     #func.now() uses the database's internal clock.
     #sever um user um diff place il annel, datetime.now() time confusin undakkum.
-    applied_at=Column(DataTime(timezone=True),sever_default=func.now()) [cite:16]
-    status=Column(String(20), default='Applied') [cite:16]
+    applied_at=Column(DateTime(timezone=True),server_default=func.now()) 
+    status=Column(String(20), default='Applied') 
