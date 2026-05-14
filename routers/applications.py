b4d9cf583760178cefc17fb4b4c46b711d164860
Sreehari-4Git <sreeharibijukumar0acc4pub@gmail.com>
2026-05-14 21:58:29 +0530

    ksksk

--- routers/applications.py
+++ routers/applications.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Depends,status
+from fastapi import APIRouter, HTTPException, Depends, status
 from sqlalchemy.orm import Session
 from typing import List
 from database import get_db
